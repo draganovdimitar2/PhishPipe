bd2f9ee1a68f959d1c7a4a47f6e69ff069443592
Dimitar Georgiev Draganov <draganovdimitar2@gmail.com>
2026-02-06 15:14:34 +0200

    feat: add publisher task to phishing_pipeline DAG

--- dags/phishing_pipeline.py
+++ dags/phishing_pipeline.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from airflow import DAG
 from operators.phishing_getter import PhishingGetterOperator
-from operators.change_verifier import ChangeVerifier
-from config import PHISHING_FEED_URL, CURRENT_FILE, PREVIOUS_FILE
+from operators.change_verifier import ChangeVerifierOperator
+from operators.s3_publisher import S3PublisherOperator
+from config import PHISHING_FEED_URL, PHISHING_CURRENT_FILE_PATH, PHISHING_PREVIOUS_FILE_PATH
 
 default_args = {
     'owner': 'airflow',
@@ -11,21 +12,27 @@ default_args = {
 }
 
 with DAG(
-    dag_id='phishing_pipeline',
-    default_args=default_args,
-    schedule_interval='@daily',
-    catchup=False
+        dag_id='phishing_pipeline',
+        default_args=default_args,
+        schedule_interval='@daily',
+        catchup=False
 ) as dag:
-
     downloader = PhishingGetterOperator(
         task_id='downloader',
         url=PHISHING_FEED_URL,
-        output_path=CURRENT_FILE
+        output_path=PHISHING_CURRENT_FILE_PATH
     )
-    change_verifier = ChangeVerifier(
+    change_verifier = ChangeVerifierOperator(
         task_id='change_verifier',
-        current_file=CURRENT_FILE,
-        previous_file=PREVIOUS_FILE
+        current_file=PHISHING_CURRENT_FILE_PATH,
+        previous_file=PHISHING_PREVIOUS_FILE_PATH
     )
 
-    downloader >> change_verifier
+    publisher = S3PublisherOperator(
+        task_id="publisher",
+        bucket_name="phishpipe-bucket",
+        paths=[PHISHING_CURRENT_FILE_PATH],
+        s3_prefix="phishing",
+    )
+
+    downloader >> change_verifier >> publisher
