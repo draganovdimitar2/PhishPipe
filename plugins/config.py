116d1d0b80484a419c88290dafeffdda6e762b7f
Dimitar Georgiev Draganov <draganovdimitar2@gmail.com>
2026-02-13 15:08:04 +0200

    refactor(config): add PHISHING_CURRENT_HASH_VARIABLE_KEY and PHISHING_PREVIOUS_HASH_VARIABLE_KEY to config.py

--- plugins/config.py
+++ plugins/config.py
@@ -5,3 +5,7 @@ PHISHING_FEED_URL = "http://svn.code.sf.net/p/aper/code/phishing_reply_addresses
 DATA_DIR = "/opt/airflow/data"
 PHISHING_CURRENT_FILE_PATH = f"{DATA_DIR}/phishing_current.csv"
 PHISHING_PREVIOUS_FILE_PATH = f"{DATA_DIR}/phishing_previous.csv"
+
+# Airflow Variable keys (persisted in Airflow metadata DB)
+PHISHING_CURRENT_HASH_VARIABLE_KEY = "phishing_current_hash"
+PHISHING_PREVIOUS_HASH_VARIABLE_KEY = "phishing_previous_hash"
